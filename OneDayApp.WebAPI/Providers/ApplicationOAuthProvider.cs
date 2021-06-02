eecaf8372647686ec737a67e7a86da4884325026
Ryan <Dibzthedibz@gmail.com>
2021-06-01 18:40:06 -0700

    CommentListItem and CommentCreate populated.

--- OneDayApp.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ OneDayApp.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
-using OneDayApp.Data;
-using OneDayApp.WebAPI.Models;
+using OneDay.Data;
 
 namespace OneDayApp.WebAPI.Providers
 {
